e7c5cbed515d7cb7eaf3cdb0f3519c7fec92dc97
Pedro Andres Chaparro Quintero <pedroandreschaparro@gmail.com>
2022-12-08 17:38:01 -0500

    :sparkles: `Footer` completed

---
+++ easybank-landing-page/src/components/Footer/Footer.tsx
@@ -0,0 +1,61 @@
+import { GradientButton } from '../GradientButton/GradiendButton';
+import Styles from './Footer.module.css';
+
+export const Footer = () => {
+  return (
+    <footer className={Styles['footer-background']}>
+      <div className={`${Styles.footer} container`}>
+        <div className={`${Styles.footer__column} ${Styles['footer__column-spaced']}`}>
+          <img src='/images/logo-white.svg' alt='Easybank logo' />
+          <div className={Styles.footer__socials}>
+            <a href='#' target='_blank' rel='noopener noreferrer'>
+              <img src='/icons/icon-facebook.svg' alt='Find Easybank on facebook' />
+            </a>
+            <a href='#' target='_blank' rel='noopener noreferrer'>
+              <img src='/icons/icon-youtube.svg' alt='Find Easybank on youtube' />
+            </a>
+            <a href='#' target='_blank' rel='noopener noreferrer'>
+              <img src='/icons/icon-twitter.svg' alt='Find Easybank on twitter' />
+            </a>
+            <a href='#' target='_blank' rel='noopener noreferrer'>
+              <img src='/icons/icon-pinterest.svg' alt='Find Easybank on pinterest' />
+            </a>
+            <a href='#' target='_blank' rel='noopener noreferrer'>
+              <img src='/icons/icon-instagram.svg' alt='Find Easybank on intagram' />
+            </a>
+          </div>
+        </div>
+        <div className={Styles.footer__column}>
+          <ul className={Styles.footer__options}>
+            <li>
+              <a href='#'>About Us</a>
+            </li>
+            <li>
+              <a href='#'>Contact</a>
+            </li>
+            <li>
+              <a href='#'>Blog</a>
+            </li>
+          </ul>
+        </div>
+        <div className={Styles.footer__column}>
+          <ul className={Styles.footer__options}>
+            <li>
+              <a href='#'>Careers</a>
+            </li>
+            <li>
+              <a href='#'>Suport</a>
+            </li>
+            <li>
+              <a href='#'>Provacy Policy</a>
+            </li>
+          </ul>
+        </div>
+        <div className={`${Styles.footer__column} ${Styles['footer__column-spaced']}`}>
+          <GradientButton text='Request invite' />
+          <p className={Styles.footer__copyright}>© Easybank. All Rights Reserved</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
--- easybank-landing-page/src/main.tsx
+++ easybank-landing-page/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Footer } from './components/Footer/Footer';
 import { Navigation } from './components/Navigation/Navigation';
 import './index.css';
 import { HomePage } from './pages/HomePage/HomePage';
@@ -8,5 +9,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Navigation />
     <HomePage />
+    <Footer />
   </React.StrictMode>,
 );
