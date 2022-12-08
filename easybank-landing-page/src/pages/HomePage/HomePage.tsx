256acb14e766e128beb801a0c4f00f6a0ca73e3d
Pedro Andres Chaparro Quintero <pedroandreschaparro@gmail.com>
2022-12-08 09:53:08 -0500

    :sparkles: Hero images

--- easybank-landing-page/src/pages/HomePage/HomePage.tsx
+++ easybank-landing-page/src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,12 @@
 import { Hero } from '../../components/Hero/Hero';
 
+import Styles from './HomePage.module.css';
+
 export const HomePage = () => {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <section className={Styles['space-placeholder']}></section>
+    </>
+  );
 };
